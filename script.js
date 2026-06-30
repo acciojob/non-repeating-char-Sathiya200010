92b8f53654eaf19b7947b4a1abeba9f5b5d29f4a
Sathiya R <rskeerthi01@gmail.com>
2026-06-30 19:26:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function firstNonRepeatedChar(str) {
 let res="";
-for(let i=0;i<=str.length;i++){
+for(let i=0;i<str.length;i++){
 	let count=0;
-	for(let j=0;j<=str.length;j++){
+	for(let j=0;j<str.length;j++){
 		if(str[i]===str[j]){
 			count++;
 		}
-	}if(count==1){
+	}if(count===1){
 		return str[i];
 	}
 }return null;
