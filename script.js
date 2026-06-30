c961af0f43ef44880e8ff6adc7971d71fe33b586
Sathiya R <rskeerthi01@gmail.com>
2026-06-30 19:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+let res="";
+for(let i=0;i<=str.length;i++){
+	let count=0;
+	for(let j=0;j<=str.length;j++){
+		if(str[i]===str[j]){
+			count++;
+		}
+	}if(count==1){
+		return str[i];
+	}
+}return null;
+		// Write your code here
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
